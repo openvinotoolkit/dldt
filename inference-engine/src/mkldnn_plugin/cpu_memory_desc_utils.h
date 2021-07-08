052f884ab7bf8d941c073d1e15d948befd817df5
Maksim Kutakov <maksim.kutakov@intel.com>
2021-07-08 10:41:40 +0300

    Unified memory descriptor working state static.

---
+++ inference-engine/src/mkldnn_plugin/cpu_memory_desc_utils.h
@@ -0,0 +1,29 @@
+// Copyright (C) 2018-2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <ie_layouts.h>
+#include <ie_blob.h>
+
+namespace MKLDNNPlugin {
+class MKLDNNMemoryDesc;
+class BlockedMemoryDesc;
+class MKLDNNMemory;
+
+class MemoryDescUtils {
+public:
+    static InferenceEngine::TensorDesc convertToTensorDesc(const MemoryDesc& desc);
+    static MKLDNNMemoryDesc convertToMKLDNNMemoryDesc(const MemoryDesc& desc);
+    static MKLDNNMemoryDesc convertToMKLDNNMemoryDesc(const BlockedMemoryDesc& desc);
+    static MKLDNNMemoryDesc convertToMKLDNNMemoryDesc(const InferenceEngine::TensorDesc& desc);
+    static BlockedMemoryDesc convertToBlockedDescriptor(const MemoryDesc& desc);
+    static BlockedMemoryDesc convertToBlockedDescriptor(const MKLDNNMemoryDesc& inpDesc);
+    static MemoryDescPtr applyUndefinedOffset(const MKLDNNMemoryDesc& desc);
+    static MemoryDescPtr applyUndefinedOffset(const BlockedMemoryDesc& desc);
+    static MemoryDescPtr resetOffset(const MemoryDesc* desc);
+    static InferenceEngine::Blob::Ptr interpretAsBlob(const MKLDNNMemory& mem);
+};
+
+}  // namespace MKLDNNPlugin
