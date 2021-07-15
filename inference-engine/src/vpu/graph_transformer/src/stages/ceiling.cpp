KorA6/openvino

[VPU] switch CNNLayer -> ngraph

inference-engine/src/vpu/graph_transformer/src/stages/ceiling.cpp
@@ -28,16 +28,16 @@ private:
 
 }  // namespace
 
-void FrontEnd::parseCeiling(const Model& model, const ie::CNNLayerPtr& layer, const DataVector& inputs, const DataVector& outputs) const {
+void FrontEnd::parseCeiling(const Model& model, const NodePtr& node, const DataVector& inputs, const DataVector& outputs) const {
     VPU_THROW_UNLESS(inputs.size() == 1,
                      "Ceiling stage with name {} must have only 1 input, actually provided {} inputs",
-                     layer->name, inputs.size());
+                     node->get_name(), inputs.size());
 
     VPU_THROW_UNLESS(outputs.size() == 1,
                      "Ceiling stage with name {} must have only 1 output, actually provided {} outputs",
-                     layer->name, outputs.size());
+                     node->get_name(), outputs.size());
 
-    model->addNewStage<CeilingStage>(layer->name, StageType::Ceiling, layer, inputs, outputs);
+    model->addNewStage<CeilingStage>(node->get_name(), StageType::Ceiling, node, inputs, outputs);
 }
 
 }  // namespace vpu

inference-engine/src/vpu/graph_transformer/src/stages/exp_generateproposals.cpp
@@ -70,18 +70,19 @@ private:
 
 void FrontEnd::parseExpGenerateProposals(
         const Model& model,
-        const ie::CNNLayerPtr& layer,
+        const NodePtr& node,
         const DataVector& inputs,
         const DataVector& outputs) const {
-    VPU_THROW_UNLESS(inputs.size() == 4, "Layer %s must have 4 input tensors.", layer->name);
-    VPU_THROW_UNLESS(outputs.size() == 2, "Layer %s must have 2 output tensors.", layer->name);
+    auto expGenerateProposals = ngraph::as_type_ptr<ngraph::op::v6::ExperimentalDetectronGenerateProposalsSingleImage>(node);
+    VPU_THROW_UNLESS(inputs.size() == 4, "Layer %s must have 4 input tensors.", expGenerateProposals->get_name());
+    VPU_THROW_UNLESS(outputs.size() == 2, "Layer %s must have 2 output tensors.", expGenerateProposals->get_name());
 
     ExpGenerateProposalsParams params;
-
-    params.min_size      = layer->GetParamAsFloat("min_size", 0.0f);
-    params.nms_threshold = layer->GetParamAsFloat("nms_threshold", 0.7f);
-    params.pre_nms_topn  = layer->GetParamAsInt("pre_nms_count", 1000);
-    params.post_nms_topn = layer->GetParamAsInt("post_nms_count", 1000);
+    const auto attrs = expGenerateProposals->get_attrs();
+    params.min_size      = attrs.min_size;
+    params.nms_threshold = attrs.nms_threshold;
+    params.pre_nms_topn  = attrs.pre_nms_count;
+    params.post_nms_topn = attrs.post_nms_count;
 
     auto imInfo       = inputs[0];
     auto inputAnchors = inputs[1];
@@ -93,41 +94,41 @@ void FrontEnd::parseExpGenerateProposals(
     VPU_THROW_UNLESS((inputAnchors->desc().dims().size() == 2) &&
                      (inputAnchors->desc().dim(Dim::C) == 4),
                      "Wrong shape for input 1 of layer %s, expected (N, 4), got: dims size = %lu, dim C = %d",
-                     layer->name, inputAnchors->desc().dims().size(), inputAnchors->desc().dim(Dim::C));
+                     expGenerateProposals->get_name(), inputAnchors->desc().dims().size(), inputAnchors->desc().dim(Dim::C));
     VPU_THROW_UNLESS((imInfo->desc().dims().size() == 1) &&
                      (imInfo->desc().dim(Dim::C) == 3),
                      "Wrong shape for input 0 of layer %s, expected (3), got: dims size = %lu, dim C = %d",
-                     layer->name, imInfo->desc().dims().size(), imInfo->desc().dim(Dim::C));
+                     expGenerateProposals->get_name(), imInfo->desc().dims().size(), imInfo->desc().dim(Dim::C));
 
     VPU_THROW_UNLESS(inputDeltas->desc().dims().size() == 3,
                      "Wrong shape for input 2 of layer %s, expected dim size = 3, got: %lu",
-                     layer->name, inputDeltas->desc().dims().size());
+                     expGenerateProposals->get_name(), inputDeltas->desc().dims().size());
     VPU_THROW_UNLESS(inputScores->desc().dims().size() == 3,
                      "Wrong shape for input 3 of layer %s, expected dim size = 3, got: %lu",
-                     layer->name, inputScores->desc().dims().size());
+                     expGenerateProposals->get_name(), inputScores->desc().dims().size());
 
     VPU_THROW_UNLESS((inputDeltas->desc().dim(Dim::H) == inputScores->desc().dim(Dim::H)) &&
                      (inputDeltas->desc().dim(Dim::W) == inputScores->desc().dim(Dim::W)),
                      "Inputs 2 and 3 of layer %s must have same H and W, got: input2 (H = %d, W = %d), input3 (H = %d, W = %d)",
-                     layer->name, inputDeltas->desc().dim(Dim::H), inputDeltas->desc().dim(Dim::W),
+                     expGenerateProposals->get_name(), inputDeltas->desc().dim(Dim::H), inputDeltas->desc().dim(Dim::W),
                      inputScores->desc().dim(Dim::H), inputScores->desc().dim(Dim::W));
 
     VPU_THROW_UNLESS((outputRois->desc().dims().size() == 2) &&
                      (outputRois->desc().dim(Dim::C) == 4),
                      "Wrong shape for output 0 of layer %s, expected (N, 4), got: dims size = %lu, dim C = %d",
-                     layer->name, outputRois->desc().dims().size(), outputRois->desc().dim(Dim::C));
+                     expGenerateProposals->get_name(), outputRois->desc().dims().size(), outputRois->desc().dim(Dim::C));
     VPU_THROW_UNLESS(outputScores->desc().dims().size() == 1,
                      "Wrong shape for output 1 of layer %s, expected dim size = 1, got: %lu",
-                     layer->name, outputScores->desc().dims().size());
+                     expGenerateProposals->get_name(), outputScores->desc().dims().size());
 
     VPU_THROW_UNLESS(outputRois->desc().dim(Dim::N) == outputScores->desc().dim(Dim::C),
                      "Layer %s: output0 dim N and output1 dim C must be equal, got: output0 (N = %d), output1 (C = %d)",
-                     layer->name, outputRois->desc().dim(Dim::N), outputScores->desc().dim(Dim::C));
+                     expGenerateProposals->get_name(), outputRois->desc().dim(Dim::N), outputScores->desc().dim(Dim::C));
 
     auto stage = model->addNewStage<ExpGenerateProposalsStage>(
-        layer->name,
+        expGenerateProposals->get_name(),
         StageType::ExpGenerateProposals,
-        layer,
+        expGenerateProposals,
         inputs,
         outputs);
 

inference-engine/src/vpu/graph_transformer/src/stages/priorbox.cpp
@@ -55,18 +55,18 @@ private:
 
 }  // namespace
 
-void FrontEnd::parsePriorBox(const Model& model, const ie::CNNLayerPtr& layer, const DataVector& inputs, const DataVector& outputs) const {
+void FrontEnd::parsePriorBox(const Model& model, const NodePtr& node, const DataVector& inputs, const DataVector& outputs) const {
     IE_ASSERT(inputs.size() == 2);
     IE_ASSERT(outputs.size() == 1);
 
-    model->addNewStage<StubPriorBoxStage>(layer->name, StageType::StubPriorBox, layer, inputs, outputs);
+    model->addNewStage<StubPriorBoxStage>(node->get_name(), StageType::StubPriorBox, node, inputs, outputs);
 }
 
-void FrontEnd::parsePriorBoxClustered(const Model& model, const ie::CNNLayerPtr& layer, const DataVector& inputs, const DataVector& outputs) const {
+void FrontEnd::parsePriorBoxClustered(const Model& model, const NodePtr& node, const DataVector& inputs, const DataVector& outputs) const {
     IE_ASSERT(inputs.size() == 2);
     IE_ASSERT(outputs.size() == 1);
 
-    model->addNewStage<StubPriorBoxStage>(layer->name, StageType::StubPriorBoxClustered, layer, inputs, outputs);
+    model->addNewStage<StubPriorBoxStage>(node->get_name(), StageType::StubPriorBoxClustered, node, inputs, outputs);
 }
 
 }  // namespace vpu

inference-engine/src/vpu/graph_transformer/src/stages/reorg_yolo.cpp
@@ -68,18 +68,19 @@ private:
 
 }  // namespace
 
-void FrontEnd::parseReorgYolo(const Model& model, const ie::CNNLayerPtr& layer, const DataVector& inputs, const DataVector& outputs) const {
+void FrontEnd::parseReorgYolo(const Model& model, const NodePtr& node, const DataVector& inputs, const DataVector& outputs) const {
     IE_ASSERT(inputs.size() == 1);
     IE_ASSERT(outputs.size() == 1);
-
+    const auto& reorgYolo = ngraph::as_type_ptr<ngraph::op::ReorgYolo>(node);
+    IE_ASSERT(reorgYolo != nullptr);
     auto stage = model->addNewStage<ReorgYoloStage>(
-        layer->name,
+        reorgYolo->get_name(),
         StageType::ReorgYolo,
-        layer,
+        reorgYolo,
         inputs,
         outputs);
 
-    stage->attrs().set<int>("stride", layer->GetParamAsInt("stride", 2));
+    stage->attrs().set<int>("stride", reorgYolo->get_strides()[0]);  // add some condition?
 }
 
 }  // namespace vpu

openvinotoolkit/dldt

inference-engine/src/vpu/graph_transformer/src/stages/ceiling.cpp
@@ -28,16 +28,16 @@ private:
 
 }  // namespace
 
-void FrontEnd::parseCeiling(const Model& model, const ie::CNNLayerPtr& layer, const DataVector& inputs, const DataVector& outputs) const {
+void FrontEnd::parseCeiling(const Model& model, const NodePtr& node, const DataVector& inputs, const DataVector& outputs) const {
     VPU_THROW_UNLESS(inputs.size() == 1,
                      "Ceiling stage with name {} must have only 1 input, actually provided {} inputs",
-                     layer->name, inputs.size());
+                     node->get_friendly_name(), inputs.size());
 
     VPU_THROW_UNLESS(outputs.size() == 1,
                      "Ceiling stage with name {} must have only 1 output, actually provided {} outputs",
-                     layer->name, outputs.size());
+                     node->get_friendly_name(), outputs.size());
 
-    model->addNewStage<CeilingStage>(layer->name, StageType::Ceiling, layer, inputs, outputs);
+    model->addNewStage<CeilingStage>(node->get_friendly_name(), StageType::Ceiling, node, inputs, outputs);
 }
 
 }  // namespace vpu

inference-engine/src/vpu/graph_transformer/src/stages/exp_generateproposals.cpp
@@ -70,18 +70,19 @@ private:
 
 void FrontEnd::parseExpGenerateProposals(
         const Model& model,
-        const ie::CNNLayerPtr& layer,
+        const NodePtr& node,
         const DataVector& inputs,
         const DataVector& outputs) const {
-    VPU_THROW_UNLESS(inputs.size() == 4, "Layer %s must have 4 input tensors.", layer->name);
-    VPU_THROW_UNLESS(outputs.size() == 2, "Layer %s must have 2 output tensors.", layer->name);
+    auto expGenerateProposals = ngraph::as_type_ptr<ngraph::op::v6::ExperimentalDetectronGenerateProposalsSingleImage>(node);
+    VPU_THROW_UNLESS(inputs.size() == 4, "Layer %s must have 4 input tensors.", expGenerateProposals->get_friendly_name());
+    VPU_THROW_UNLESS(outputs.size() == 2, "Layer %s must have 2 output tensors.", expGenerateProposals->get_friendly_name());
 
     ExpGenerateProposalsParams params;
-
-    params.min_size      = layer->GetParamAsFloat("min_size", 0.0f);
-    params.nms_threshold = layer->GetParamAsFloat("nms_threshold", 0.7f);
-    params.pre_nms_topn  = layer->GetParamAsInt("pre_nms_count", 1000);
-    params.post_nms_topn = layer->GetParamAsInt("post_nms_count", 1000);
+    const auto attrs = expGenerateProposals->get_attrs();
+    params.min_size      = attrs.min_size;
+    params.nms_threshold = attrs.nms_threshold;
+    params.pre_nms_topn  = attrs.pre_nms_count;
+    params.post_nms_topn = attrs.post_nms_count;
 
     auto imInfo       = inputs[0];
     auto inputAnchors = inputs[1];
@@ -93,41 +94,41 @@ void FrontEnd::parseExpGenerateProposals(
     VPU_THROW_UNLESS((inputAnchors->desc().dims().size() == 2) &&
                      (inputAnchors->desc().dim(Dim::C) == 4),
                      "Wrong shape for input 1 of layer %s, expected (N, 4), got: dims size = %lu, dim C = %d",
-                     layer->name, inputAnchors->desc().dims().size(), inputAnchors->desc().dim(Dim::C));
+                     expGenerateProposals->get_friendly_name(), inputAnchors->desc().dims().size(), inputAnchors->desc().dim(Dim::C));
     VPU_THROW_UNLESS((imInfo->desc().dims().size() == 1) &&
                      (imInfo->desc().dim(Dim::C) == 3),
                      "Wrong shape for input 0 of layer %s, expected (3), got: dims size = %lu, dim C = %d",
-                     layer->name, imInfo->desc().dims().size(), imInfo->desc().dim(Dim::C));
+                     expGenerateProposals->get_friendly_name(), imInfo->desc().dims().size(), imInfo->desc().dim(Dim::C));
 
     VPU_THROW_UNLESS(inputDeltas->desc().dims().size() == 3,
                      "Wrong shape for input 2 of layer %s, expected dim size = 3, got: %lu",
-                     layer->name, inputDeltas->desc().dims().size());
+                     expGenerateProposals->get_friendly_name(), inputDeltas->desc().dims().size());
     VPU_THROW_UNLESS(inputScores->desc().dims().size() == 3,
                      "Wrong shape for input 3 of layer %s, expected dim size = 3, got: %lu",
-                     layer->name, inputScores->desc().dims().size());
+                     expGenerateProposals->get_friendly_name(), inputScores->desc().dims().size());
 
     VPU_THROW_UNLESS((inputDeltas->desc().dim(Dim::H) == inputScores->desc().dim(Dim::H)) &&
                      (inputDeltas->desc().dim(Dim::W) == inputScores->desc().dim(Dim::W)),
                      "Inputs 2 and 3 of layer %s must have same H and W, got: input2 (H = %d, W = %d), input3 (H = %d, W = %d)",
-                     layer->name, inputDeltas->desc().dim(Dim::H), inputDeltas->desc().dim(Dim::W),
+                     expGenerateProposals->get_friendly_name(), inputDeltas->desc().dim(Dim::H), inputDeltas->desc().dim(Dim::W),
                      inputScores->desc().dim(Dim::H), inputScores->desc().dim(Dim::W));
 
     VPU_THROW_UNLESS((outputRois->desc().dims().size() == 2) &&
                      (outputRois->desc().dim(Dim::C) == 4),
                      "Wrong shape for output 0 of layer %s, expected (N, 4), got: dims size = %lu, dim C = %d",
-                     layer->name, outputRois->desc().dims().size(), outputRois->desc().dim(Dim::C));
+                     expGenerateProposals->get_friendly_name(), outputRois->desc().dims().size(), outputRois->desc().dim(Dim::C));
     VPU_THROW_UNLESS(outputScores->desc().dims().size() == 1,
                      "Wrong shape for output 1 of layer %s, expected dim size = 1, got: %lu",
-                     layer->name, outputScores->desc().dims().size());
+                     expGenerateProposals->get_friendly_name(), outputScores->desc().dims().size());
 
     VPU_THROW_UNLESS(outputRois->desc().dim(Dim::N) == outputScores->desc().dim(Dim::C),
                      "Layer %s: output0 dim N and output1 dim C must be equal, got: output0 (N = %d), output1 (C = %d)",
-                     layer->name, outputRois->desc().dim(Dim::N), outputScores->desc().dim(Dim::C));
+                     expGenerateProposals->get_friendly_name(), outputRois->desc().dim(Dim::N), outputScores->desc().dim(Dim::C));
 
     auto stage = model->addNewStage<ExpGenerateProposalsStage>(
-        layer->name,
+        expGenerateProposals->get_friendly_name(),
         StageType::ExpGenerateProposals,
-        layer,
+        expGenerateProposals,
         inputs,
         outputs);
 

inference-engine/src/vpu/graph_transformer/src/stages/priorbox.cpp
@@ -55,18 +55,18 @@ private:
 
 }  // namespace
 
-void FrontEnd::parsePriorBox(const Model& model, const ie::CNNLayerPtr& layer, const DataVector& inputs, const DataVector& outputs) const {
+void FrontEnd::parsePriorBox(const Model& model, const NodePtr& node, const DataVector& inputs, const DataVector& outputs) const {
     IE_ASSERT(inputs.size() == 2);
     IE_ASSERT(outputs.size() == 1);
 
-    model->addNewStage<StubPriorBoxStage>(layer->name, StageType::StubPriorBox, layer, inputs, outputs);
+    model->addNewStage<StubPriorBoxStage>(node->get_friendly_name(), StageType::StubPriorBox, node, inputs, outputs);
 }
 
-void FrontEnd::parsePriorBoxClustered(const Model& model, const ie::CNNLayerPtr& layer, const DataVector& inputs, const DataVector& outputs) const {
+void FrontEnd::parsePriorBoxClustered(const Model& model, const NodePtr& node, const DataVector& inputs, const DataVector& outputs) const {
     IE_ASSERT(inputs.size() == 2);
     IE_ASSERT(outputs.size() == 1);
 
-    model->addNewStage<StubPriorBoxStage>(layer->name, StageType::StubPriorBoxClustered, layer, inputs, outputs);
+    model->addNewStage<StubPriorBoxStage>(node->get_friendly_name(), StageType::StubPriorBoxClustered, node, inputs, outputs);
 }
 
 }  // namespace vpu

inference-engine/src/vpu/graph_transformer/src/stages/reorg_yolo.cpp
@@ -68,18 +68,19 @@ private:
 
 }  // namespace
 
-void FrontEnd::parseReorgYolo(const Model& model, const ie::CNNLayerPtr& layer, const DataVector& inputs, const DataVector& outputs) const {
+void FrontEnd::parseReorgYolo(const Model& model, const NodePtr& node, const DataVector& inputs, const DataVector& outputs) const {
     IE_ASSERT(inputs.size() == 1);
     IE_ASSERT(outputs.size() == 1);
-
+    const auto& reorgYolo = ngraph::as_type_ptr<ngraph::op::ReorgYolo>(node);
+    IE_ASSERT(reorgYolo != nullptr);
     auto stage = model->addNewStage<ReorgYoloStage>(
-        layer->name,
+        reorgYolo->get_friendly_name(),
         StageType::ReorgYolo,
-        layer,
+        reorgYolo,
         inputs,
         outputs);
 
-    stage->attrs().set<int>("stride", layer->GetParamAsInt("stride", 2));
+    stage->attrs().set<int>("stride", reorgYolo->get_strides()[0]);  // add some condition?
 }
 
 }  // namespace vpu