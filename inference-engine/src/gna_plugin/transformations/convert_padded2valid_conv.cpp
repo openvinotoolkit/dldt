02714541347e0bf08fb29808fd8f73f4ce07945f
Szymon Irzabek <szymon.jakub.irzabek@intel.com>
2021-07-19 11:19:55 +0200

    [GNA] Remove test line

--- inference-engine/src/gna_plugin/transformations/convert_padded2valid_conv.cpp
+++ inference-engine/src/gna_plugin/transformations/convert_padded2valid_conv.cpp
@@ -35,7 +35,7 @@ struct ConvData {
 
 static bool GetConvParams(std::shared_ptr<ngraph::opset7::Convolution> conv, ConvData& conv_data) {
     const auto& input = conv->input_value(0);
-    auto xyz = conv->get_output_shape(0);
+
     conv_data.padding_type = conv->get_auto_pad();
     conv_data.input_channel_count = conv->input_value(0).get_shape()[1];
     conv_data.input_height = conv->input_value(0).get_shape()[2];
